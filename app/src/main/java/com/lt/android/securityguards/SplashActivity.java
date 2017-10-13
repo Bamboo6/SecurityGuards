7ea9142c79c1c2f95bcd351afa343bbf270e578f
LauTou <31620080+Bamboo6@users.noreply.github.com>
2017-10-13 10:26:53 +0800

    Initial commit

---
+++ app/src/main/java/com/lt/android/securityguards/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.lt.android.securityguards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
