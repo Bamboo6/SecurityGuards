ff867b4c5125e529c002d39676d6af6d869276e1
Bamboo6 <dream1718@foxmail.com>
2017-10-20 12:19:34 +0800

    Android-检测更新（一）

--- app/src/main/java/com/lt/android/securityguards/SplashActivity.java
+++ app/src/main/java/com/lt/android/securityguards/SplashActivity.java
@@ -2,12 +2,29 @@ package com.lt.android.securityguards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lt.android.securityguards.m1home.utils.MyUtils;
+import com.lt.android.securityguards.m1home.utils.VersionUpdateUtils;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView mTvVersion;
+    private String mVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mVersion = MyUtils.getVersion(getApplicationContext());
+        mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
+        mTvVersion.setText("版本号："+mVersion);
+        final VersionUpdateUtils versionUpdateUntils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                versionUpdateUntils.getCloudVersion();
+            }
+        }.start();
     }
 }
